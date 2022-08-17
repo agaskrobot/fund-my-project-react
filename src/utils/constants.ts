808f04f7bcd61b33c1b5e93ef29d94a99f197a31
Aga <agnieszka.skrobot1@gmail.com>
2022-08-17 19:13:54 +0200

    feat: smart contract info

---
+++ src/utils/constants.ts
@@ -0,0 +1,6 @@
+import factoryABI from './FundMeFactory.json';
+import fundMeABI from './FundMe.json';
+
+export const contractFundMeABI = fundMeABI.abi;
+export const contractFactoryABI = factoryABI.abi;
+export const contractAddress = '0x6C06B1Db9ffEc411EEfC4843Ec8A0ceC1b91b61b';
